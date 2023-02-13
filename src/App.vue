b14b8299e8cb000f658c7414582b6ea88c803d72
sarjid <sarjid777gmail.com>
2023-02-13 11:11:54 +0600

    slot

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,54 @@
       Show Modal
     </button>
 
-    <Modal @close="toggleModal" :modalActive="modalActive"></Modal>
+    <Modal @close="toggleModal" :modalActive="modalActive">
+      <template #content>
+        <form class="bg-white rounded px-8 pt-6 mb-4">
+          <div class="mb-4">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="username"
+            >
+              Username
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Username"
+            />
+          </div>
+          <div class="mb-6">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="password"
+            >
+              Password
+            </label>
+            <input
+              class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Type Your Password"
+            />
+          </div>
+          <div class="flex items-center justify-between">
+            <button
+              class="bg-green-800 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Sign In
+            </button>
+            <a
+              class="inline-block align-baseline font-bold text-sm text-red-500 hover:text-red-600"
+              href="#"
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+      </template>
+    </Modal>
   </div>
 </template>
 
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,12 +1,12 @@
 <template>
   <div v-show="modalActive">
-    <transition name="modal-animation">
+    <transition name="fade">
       <div class="fixed z-10 overflow-y-auto top-0 w-full left-0">
         <div
           class="flex items-center justify-center min-height-100vh pt-4 px-4 pb-20 text-center sm:block sm:p-0"
         >
           <div class="fixed inset-0 transition-opacity" @click="closeModal">
-            <div class="absolute inset-0 bg-gray-900 opacity-75" />
+            <div class="absolute inset-0 bg-gray-900 opacity-75"></div>
           </div>
           <span class="hidden sm:inline-block sm:align-middle sm:h-screen"
             >&#8203;</span
@@ -17,50 +17,7 @@
             aria-modal="true"
             aria-labelledby="modal-headline"
           >
-            <form class="bg-white rounded px-8 pt-6 mb-4">
-              <div class="mb-4">
-                <label
-                  class="block text-gray-700 text-sm font-bold mb-2"
-                  for="username"
-                >
-                  Username
-                </label>
-                <input
-                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="username"
-                  type="text"
-                  placeholder="Username"
-                />
-              </div>
-              <div class="mb-6">
-                <label
-                  class="block text-gray-700 text-sm font-bold mb-2"
-                  for="password"
-                >
-                  Password
-                </label>
-                <input
-                  class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                  id="password"
-                  type="password"
-                  placeholder="Type Your Password"
-                />
-              </div>
-              <div class="flex items-center justify-between">
-                <button
-                  class="bg-green-800 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline"
-                  type="button"
-                >
-                  Sign In
-                </button>
-                <a
-                  class="inline-block align-baseline font-bold text-sm text-red-500 hover:text-red-600"
-                  href="#"
-                >
-                  Forgot Password?
-                </a>
-              </div>
-            </form>
+            <slot name="content"></slot>
             <div class="px-4 py-3 text-right">
               <button
                 type="button"
@@ -93,30 +50,30 @@ const closeModal = () => {
 </script>
 
 <style scoped>
-.modal-animation-enter-active,
-.modal-animation-leave-active {
+.fade-enter-active,
+.fade-leave-active {
   transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
 }
 
-.modal-animation-enter-from,
-.modal-animation-leave-to {
+.fade-enter-from,
+.fade-leave-to {
   opacity: 0;
 }
 
-.modal-animation-inner-enter-active {
+.fade-inner-enter-active {
   transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
 }
 
-.modal-animation-inner-leave-active {
+.fade-inner-leave-active {
   transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
 }
 
-.modal-animation-inner-enter-from {
+.fade-inner-enter-from {
   opacity: 0;
   transform: scale(0.8);
 }
 
-.modal-animation-inner-leave-to {
+.fade-inner-leave-to {
   transform: scale(0.8);
 }
 </style>
